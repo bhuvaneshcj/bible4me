74399b5ab92b0a21bb9f1a8fa2b457fdcbd8f2a4
Bhuvanesh C <cjbhuvanesh8@gmail.com>
2025-11-22 01:16:07 +0530

    fixed build issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,13 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import { ReadingComponent } from "./components/reading/reading.component";
-import { BookSelectorComponent } from "./components/book-selector/book-selector.component";
 import { FontSizeControlComponent } from "./components/font-size-control/font-size-control.component";
 import { ChapterNavigationComponent } from "./components/chapter-navigation/chapter-navigation.component";
 import { ThemeToggleComponent } from "./components/theme-toggle/theme-toggle.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @NgModule({
-    declarations: [AppComponent, ReadingComponent, BookSelectorComponent, FontSizeControlComponent, ChapterNavigationComponent, ThemeToggleComponent, SidebarComponent],
+    declarations: [AppComponent, ReadingComponent, FontSizeControlComponent, ChapterNavigationComponent, ThemeToggleComponent, SidebarComponent],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
